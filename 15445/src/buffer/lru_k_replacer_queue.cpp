84751498b212741c56b1e685bd39cfea87eb3e86
Tien Dung <dungtn@gmail.com>
2022-12-20 08:16:51 +0700

    try homemade queue for lru_k_replacer but not better

---
+++ 15445/src/buffer/lru_k_replacer_queue.cpp
@@ -0,0 +1,141 @@
+//===----------------------------------------------------------------------===//
+//
+//                         BusTub
+//
+// lru_k_replacer.cpp
+//
+// Identification: src/buffer/lru_k_replacer.cpp
+//
+// Copyright (c) 2015-2022, Carnegie Mellon University Database Group
+//
+//===----------------------------------------------------------------------===//
+
+#include <algorithm>
+#include <iostream>
+#include "buffer/lru_k_replacer.h"
+#include "common/logger.h"
+
+namespace bustub {
+
+LRUKReplacer::LRUKReplacer(size_t num_frames, size_t k) : replacer_size_(num_frames), k_(k) {
+  frame_entries_ = new FrameEntry[num_frames];
+  history_queue_ = new Queue(2048 * num_frames);
+  cache_queue_ = new Queue(2048 * num_frames);
+}
+
+void LRUKReplacer::RecordAccess(frame_id_t frame_id) {
+  std::scoped_lock<std::mutex> lock(latch_);
+  // * You can also use BUSTUB_ASSERT to abort the process if frame id is invalid.
+  BUSTUB_ASSERT(frame_id < static_cast<frame_id_t>(replacer_size_), "Invalid frame_id");
+
+  FrameEntry *frame_entry = &frame_entries_[frame_id];
+  frame_entry->is_active_ = true;
+  size_t updated_hits_count = ++frame_entry->hits_count_;
+
+  assert(updated_hits_count > 0);
+  assert(frame_entries_[frame_id].hits_count_ > 0);
+
+  if (updated_hits_count == 1) {  // first time
+    frame_entry->pos_ = history_queue_->Insert(frame_id);
+    if (frame_entry->evictable_) {
+      curr_size_++;
+      curr_history_size_++;
+    }
+
+    assert(frame_entries_[frame_id].hits_count_ == 1);
+    assert(frame_entries_[frame_id].evictable_);
+
+  } else if (updated_hits_count == k_) {
+    // move from history_queue_ to cache_queue_
+    history_queue_->Evict(frame_entry->pos_);
+    if (frame_entry->evictable_) {
+      curr_history_size_--;
+    }
+    // add to begin of the cache_queue_
+    frame_entry->pos_ = cache_queue_->Insert(frame_id);
+
+  } else if (updated_hits_count > k_) {
+    if (frame_entry->pos_ != cache_queue_->end_ - 1) {
+      // switch to begin of the cache_queue_
+      cache_queue_->Evict(frame_entry->pos_);
+      frame_entry->pos_ = cache_queue_->Insert(frame_id);
+    }
+  }
+}
+
+void LRUKReplacer::SetEvictable(frame_id_t frame_id, bool set_evictable) {
+  std::scoped_lock<std::mutex> lock(latch_);
+  BUSTUB_ASSERT(frame_id < static_cast<frame_id_t>(replacer_size_), "Invalid frame_id");
+
+  auto frame = frame_entries_[frame_id];
+  bool hit_before = (frame.is_active_ && frame.hits_count_ > 0);
+  auto previously_is_evictable = hit_before && frame.evictable_;
+  auto currently_is_evictable = hit_before && set_evictable;
+  frame_entries_[frame_id].evictable_ = set_evictable;
+
+  if (!previously_is_evictable && currently_is_evictable) {
+    curr_size_++;
+    if (frame.hits_count_ < k_) {  // in history_queue_
+      curr_history_size_++;
+    }
+  } else if (previously_is_evictable && !currently_is_evictable) {
+    curr_size_--;
+    if (frame.hits_count_ < k_) {  // in history_queue_
+      curr_history_size_--;
+    }
+  }
+}
+
+auto LRUKReplacer::Evict(frame_id_t *frame_id) -> bool {
+  std::scoped_lock<std::mutex> lock(latch_);
+  return EvictInternal(frame_id);
+}
+auto LRUKReplacer::EvictInternal(frame_id_t *frame_id) -> bool {
+  if (curr_size_ == 0) {
+    return false;
+  }
+
+  bool evict_from_history = (curr_history_size_ > 0);
+  auto &queue = evict_from_history ? history_queue_ : cache_queue_;
+  size_t i = queue->begin_;
+  while (queue->Get(i) == -1 || !frame_entries_[queue->Get(i)].evictable_) {
+    i++;
+  }
+
+  *frame_id = queue->Get(i);
+  frame_entries_[*frame_id].is_active_ = false;
+  frame_entries_[*frame_id].hits_count_ = 0;
+  frame_entries_[*frame_id].evictable_ = true;
+
+  queue->Evict(i);
+  curr_history_size_ -= evict_from_history;  // NOLINT
+  curr_size_--;
+  return true;
+}
+
+void LRUKReplacer::Remove(frame_id_t frame_id) {
+  std::scoped_lock<std::mutex> lock(latch_);
+
+  assert(frame_id < static_cast<frame_id_t>(replacer_size_));
+  auto frame_entry = &frame_entries_[frame_id];
+  if (frame_entry->is_active_) {
+    BUSTUB_ASSERT(frame_entry->evictable_, "Can't remove an inevictable frame.");
+    if (frame_entry->hits_count_ > 0 && frame_entry->hits_count_ < k_) {  // in history_queue_
+      history_queue_->Evict(frame_entry->pos_);
+      curr_history_size_--;
+      curr_size_--;
+
+    } else if (frame_entry->hits_count_ >= k_) {  // in cache_queue_
+      cache_queue_->Evict(frame_entry->pos_);
+      curr_size_--;
+    }
+
+    frame_entries_[frame_id].is_active_ = false;
+    frame_entries_[frame_id].hits_count_ = 0;
+    frame_entries_[frame_id].evictable_ = true;
+  }
+}
+
+auto LRUKReplacer::Size() -> size_t { return curr_size_; }
+
+}  // namespace bustub
---
+++ 15445/src/include/buffer/lru_k_replacer_queue.h
@@ -0,0 +1,175 @@
+//===----------------------------------------------------------------------===//
+//
+//                         BusTub
+//
+// lru_k_replacer.h
+//
+// Identification: src/include/buffer/lru_k_replacer.h
+//
+// Copyright (c) 2015-2022, Carnegie Mellon University Database Group
+//
+//===----------------------------------------------------------------------===//
+
+#pragma once
+
+#include <limits>
+#include <mutex>  // NOLINT
+
+#include "common/config.h"
+#include "common/macros.h"
+
+namespace bustub {
+
+/**
+ * LRUKReplacer implements the LRU-k replacement policy.
+ *
+ * The LRU-k algorithm evicts a frame whose backward k-distance is maximum
+ * of all frames. Backward k-distance is computed as the difference in time between
+ * current timestamp and the timestamp of kth previous access.
+ *
+ * A frame with less than k historical references is given
+ * +inf as its backward k-distance. When multiple frames have +inf backward k-distance,
+ * classical LRU algorithm is used to choose victim.
+ */
+class LRUKReplacer {
+ public:
+  /**
+   * @brief a new LRUKReplacer.
+   * @param num_frames the maximum number of frames the LRUReplacer will be required to store
+   */
+  explicit LRUKReplacer(size_t num_frames, size_t k);
+
+  DISALLOW_COPY_AND_MOVE(LRUKReplacer);
+
+  /**
+   * @brief Destroys the LRUReplacer.
+   */
+  ~LRUKReplacer() {
+    delete[] frame_entries_;
+    delete history_queue_;
+    delete cache_queue_;
+  }
+
+  /**
+   * @brief Record the event that the given frame id is accessed at current timestamp.
+   * Create a new entry for access history if frame id has not been seen before.
+   *
+   * If frame id is invalid (ie. larger than replacer_size_), throw an exception. You can
+   * also use BUSTUB_ASSERT to abort the process if frame id is invalid.
+   *
+   * @param frame_id id of frame that received a new access.
+   */
+  void RecordAccess(frame_id_t frame_id);
+
+  /**
+   * @brief Toggle whether a frame is evictable or non-evictable. This function also
+   * controls replacer's size. Note that size is equal to number of evictable entries.
+   *
+   * If a frame was previously evictable and is to be set to non-evictable, then size should
+   * decrement. If a frame was previously non-evictable and is to be set to evictable,
+   * then size should increment.
+   *
+   * If frame id is invalid, throw an exception or abort the process.
+   *
+   * For other scenarios, this function should terminate without modifying anything.
+   *
+   * @param frame_id id of frame whose 'evictable' status will be modified
+   * @param set_evictable whether the given frame is evictable or not
+   */
+  void SetEvictable(frame_id_t frame_id, bool set_evictable);
+
+  /**
+   * @brief Find the frame with largest backward k-distance and evict that frame. Only frames
+   * that are marked as 'evictable' are candidates for eviction.
+   *
+   * A frame with less than k historical references is given +inf as its backward k-distance.
+   * If multiple frames have inf backward k-distance, then evict the frame with the earliest
+   * timestamp overall.
+   *
+   * Successful eviction of a frame should decrement the size of replacer and remove the frame's
+   * access history.
+   *
+   * @param[out] frame_id id of frame that is evicted.
+   * @return true if a frame is evicted successfully, false if no frames can be evicted.
+   */
+  auto Evict(frame_id_t *frame_id) -> bool;
+
+  /**
+   * @brief Remove an evictable frame from replacer, along with its access history.
+   * This function should also decrement replacer's size if removal is successful.
+   *
+   * Note that this is different from evicting a frame, which always remove the frame
+   * with largest backward k-distance. This function removes specified frame id,
+   * no matter what its backward k-distance is.
+   *
+   * If Remove is called on a non-evictable frame, throw an exception or abort the
+   * process.
+   *
+   * If specified frame is not found, directly return from this function.
+   *
+   * @param frame_id id of frame to be removed
+   */
+  void Remove(frame_id_t frame_id);
+
+  /**
+   * @brief Return replacer's size, which tracks the number of evictable frames.
+   *
+   * @return size_t
+   */
+  auto Size() -> size_t;
+
+  class Queue {
+   public:
+    explicit Queue(size_t size) : size_(size) {
+      queue_ = new frame_id_t[size];
+      assert(begin_ == 0 && end_ == 0);
+    }
+
+    ~Queue() { delete[] queue_; }
+
+    inline auto Insert(frame_id_t frame_id) -> size_t {
+      assert(end_ >= 0 && end_ < size_);
+      queue_[end_] = frame_id;
+      return end_++;
+    }
+
+    inline auto Evict(size_t pos) -> void {
+      assert(pos >= 0 && pos < size_);
+      queue_[pos] = -1;
+      while (queue_[begin_] == -1 && begin_ < end_) {
+        begin_++;
+      }
+    }
+
+    inline auto Get(size_t pos) -> frame_id_t { return queue_[pos]; }
+
+    size_t size_;
+    size_t begin_{0};
+    size_t end_{0};
+    frame_id_t *queue_;
+  };
+
+ private:
+  auto EvictInternal(frame_id_t *frame_id) -> bool;
+
+  struct FrameEntry {
+    size_t hits_count_{0};
+    bool evictable_{true};
+    bool is_active_{false};
+    size_t pos_;
+  };
+
+  // std::unordered_map<frame_id_t, FrameEntry> frame_entries_; // thay bằng frame_entries_ array
+  FrameEntry *frame_entries_;  // frame_id chính là index
+  Queue *history_queue_;
+  Queue *cache_queue_;
+
+  size_t curr_history_size_{0};
+  size_t curr_size_{0};
+  size_t replacer_size_;  // = num_frames, số lượng tối đa frames mà buffer pool chứa được
+  size_t k_;
+
+  std::mutex latch_;
+};
+
+}  // namespace bustub
